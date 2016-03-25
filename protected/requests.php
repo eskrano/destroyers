2e371f12e31f36105c301047adaa7c7a404cc22f
alex <alex.priadko2014@yandex.ru>
2016-03-25 07:48:53 +0200

    Dev..

--- protected/requests.php
+++ protected/requests.php
@@ -1,22 +1,34 @@
 <?php
 
-class Requests {
-	function  __construct() {}
-
-
-	function get ($param,$type)
-	{
-
-		if ($type == 'int')
-		{
-			return isset($_GET[$param]) ? (int) abs($_GET[$param]) : null;
-		}
-
-		return isset($_GET[$param]) ? trim(htmlspecialchars($_GET[$param])) : null;
-	}
-
-	function post ($param,$type,$regulars =  null)
-	{
-		$regulars =  array()
-	}
+class Requests 
+{
+    public function input($key = null)
+    {
+        return $this->commonRequestLogic('POST',$key);
+    }
+    
+    public function get($key)
+    {
+        return $this->commonRequestLogic('GET',$key);    
+    }
+    
+    protected function commonRequestLogic($type,$key = null)
+    {
+        if (null == $key) {
+            return $this->getRequestType($type);
+        }
+        return $this->getRequestType($type)[$key];
+    }
+    
+    protected function getRequestType($type)
+    {
+        switch ($type) {
+            case 'POST':
+                return $_POST;
+                break;
+            case 'GET':
+                return $_GET;
+                break;
+        }
+    }
 }
