c3bf3156af7634066b7319f990497f0e5f32a7ae
alex <alex.priadko2014@yandex.ru>
2016-03-25 07:47:54 +0200

    Небольшие правки

--- protected/db.php
+++ protected/db.php
@@ -2,57 +2,58 @@
 
 class  db
 {
-	function __construct ($db)
+    
+	public function __construct ($db)
 	{
 		$this->db = $db;
 	}
 
-	function query($query,$args)
+	public function query($query,$args)
 	{
 		$statement = $this->db->prepare($query);
 		$statement->execute($args);
 		return $statement;
 	}
 
-	function rows($query,$args)
+	public function rows($query,$args)
 	{
 		return $this->query($query,$args)->rowCount();
 	}
 
-	function fetch ($query,$args)
+	public function fetch ($query,$args)
 	{
 		$stmt = $this->query($query,$args);
 		return $data = $stmt->fetch(PDO::FETCH_ASSOC);
 	}
 
-	function fetchAll ($query,$args)
+	public function fetchAll ($query,$args)
 	{
 		$stmt =  $this->query($query,$args);
 		return $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
 	}
 
-	function noPrepared ($query)
+	public function noPrepared ($query)
 	{
 		$stmt = $this->db->query($query);
 	}
 
-	function quote ($var)
+	public function quote ($var)
 	{
 		return $var = $this->db->quote($var);
 	}
 
-	function last()
+	public function last()
 	{
 		return $this->db->lastInsertId();
 	}
 
 
-	function offEmulate ()
+	public function offEmulate ()
 	{
 		$this->db->setAttribute( PDO::ATTR_EMULATE_PREPARES, false );
 	}
 
-	function Emulate ($param) {
+	public function Emulate ($param) {
 		$this->db->setAttribute(PDO::ATTR_EMULATE_PREPARES, $param);
 	}
 	
