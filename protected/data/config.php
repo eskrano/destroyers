425b1cb556a448065fbb7dfe53106560987793ec
alex <alex.priadko2014@yandex.ru>
2016-03-25 07:48:43 +0200

    Файлы конфигов

---
+++ protected/data/config.php
@@ -0,0 +1,10 @@
+<?php 
+
+return [
+    'dbhost'=>'localhost',
+    'dbname'=>'game.dev',
+    'dbpass'=>'',
+    'dbuser'=>'root',
+    'time'=>$_SERVER['REQUEST_TIME'],
+    'root'=>$_SERVER['DOCUMENT_ROOT']  
+];
