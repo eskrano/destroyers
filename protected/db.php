8a4d0b0cf33722f9809d468962dde370cd731e38
Eskrano <mr.eskrano@gmail.com>
2017-03-16 19:03:15 +0200

    Db class refractoring

--- protected/db.php
+++ protected/db.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Class db
+ * Simple pdo wrapper
+ */
+
 class  db
 {
     
@@ -8,6 +13,11 @@ class  db
 		$this->db = $db;
 	}
 
+    /**
+     * @param $query
+     * @param $args
+     * @return mixed
+     */
 	public function query($query,$args)
 	{
 		$statement = $this->db->prepare($query);
@@ -15,47 +25,88 @@ class  db
 		return $statement;
 	}
 
+    /**
+     * @param $query
+     * @param $args
+     * @return mixed
+     */
 	public function rows($query,$args)
 	{
 		return $this->query($query,$args)->rowCount();
 	}
 
+    /**
+     * @param $query
+     * @param $args
+     * @return mixed
+     */
 	public function fetch ($query,$args)
 	{
 		$stmt = $this->query($query,$args);
 		return $data = $stmt->fetch(PDO::FETCH_ASSOC);
 	}
 
+    /**
+     * @param $query
+     * @param $args
+     * @return mixed
+     */
 	public function fetchAll ($query,$args)
 	{
 		$stmt =  $this->query($query,$args);
 		return $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
 	}
 
+    /**
+     * @param $query
+     * @return mixed
+     */
 	public function noPrepared ($query)
 	{
-		$stmt = $this->db->query($query);
+		return $this->db->query($query);
 	}
 
+    /**
+     * @param $var
+     * @return mixed
+     */
 	public function quote ($var)
 	{
 		return $var = $this->db->quote($var);
 	}
 
+    /**
+     * @return mixed
+     */
 	public function last()
 	{
 		return $this->db->lastInsertId();
 	}
 
-
+    /**
+     * off emulating
+     * @return void
+     */
 	public function offEmulate ()
 	{
 		$this->db->setAttribute( PDO::ATTR_EMULATE_PREPARES, false );
 	}
 
+    /**
+     * @deprecated
+     * @param $param
+     */
 	public function Emulate ($param) {
-		$this->db->setAttribute(PDO::ATTR_EMULATE_PREPARES, $param);
+		return $this->onEmulate($param);
 	}
+
+    /**
+     * @param $param
+     */
+	public function onEmulate($param)
+    {
+        $this->db->setAttribute(PDO::ATTR_EMULATE_PREPARES, $param);
+    }
 	
 
 }
